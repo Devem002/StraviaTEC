50bb7dc65f1360029c73f15f2dc3eb5ff8f98d55
IsaCorQ <85208919+IsaCorQ@users.noreply.github.com>
2023-11-19 21:11:23 -0600

    Add files via upload

--- SQL_Script_Populacion.sql
+++ SQL_Script_Populacion.sql
@@ -21,22 +21,22 @@ Insert into CLASIFICACION_ACTIVIDAD (Clasificacion) values ('Senderismo')
 Insert into CLASIFICACION_ACTIVIDAD (Clasificacion) values ('Kayak')
 Insert into CLASIFICACION_ACTIVIDAD (Clasificacion) values ('Caminata')
 
-Insert into PATROCINADOR (Nmbr_comercial, Nmbr_rep_legal, Logo, Num_rep_legal) values ('VMW', 'Pedro Perico', 'Aqui va el logo xd', 11111111)
-Insert into PATROCINADOR (Nmbr_comercial, Nmbr_rep_legal, Logo, Num_rep_legal) values ('Inca Cola', 'Walter White', 'Aqui va el logo xd', 22222222)
-Insert into PATROCINADOR (Nmbr_comercial, Nmbr_rep_legal, Logo, Num_rep_legal) values ('PeraPhone', 'Gojo Satoru', 'Aqui va el logo xd', 33333333)
+Insert into PATROCINADOR (Nmbr_comercial, Nmbr_rep_legal, Logo, Num_rep_legal) values ('VMW', 'Pedro Perico', 'VMW.png', 11111111)
+Insert into PATROCINADOR (Nmbr_comercial, Nmbr_rep_legal, Logo, Num_rep_legal) values ('Inca Cola', 'Walter White', 'IncaCola.png', 22222222)
+Insert into PATROCINADOR (Nmbr_comercial, Nmbr_rep_legal, Logo, Num_rep_legal) values ('PeraPhone', 'Gojo Satoru', 'PeraPhone.png', 33333333)
 
 Insert into FONDO_ALTURA (Tipo_f_a) values ('Fondo')
 Insert into FONDO_ALTURA (Tipo_f_a) values ('Altura')
 
-Insert into CARRERA (Nombre, Fecha, Hora, Precio, Kms, Recorrido_gpx, Clase_actividad) values ('Kindora Run 2023', '27/11/2023', '7:00', 30000, 10, 'Aqui va el gpx xd', 'Correr')
+Insert into CARRERA (Nombre, Fecha, Hora, Precio, Kms_total, Recorrido_gpx, Clase_actividad) values ('Kindora Run 2023', '2023-11-27', '7:00', 30000, 10, 'Aqui va el gpx xd', 'Correr')
 
 Insert into PATROCINA_CARRERA (Nmbr_patrocinador, Nmbr_carrera) values ('VMW', 'Kindora Run 2023')
 Insert into PATROCINA_CARRERA (Nmbr_patrocinador, Nmbr_carrera) values ('PeraPhone', 'Kindora Run 2023')
 
 Insert into CUENTA_BANCO (Numero_cuenta, Carrera_dueno) values (4745, 'Kindora Run 2023')
 
-Insert into RETOS (Nombre, Kms, Fecha_inicio, Fecha_fin, Fondo_altura, Clase_actividad) values ('Reto Fin de Año Correr 2023', 100, '1/12/2023', '1/1/2024', 'Fondo', 'Correr')
-Insert into RETOS (Nombre, Kms, Fecha_inicio, Fecha_fin, Fondo_altura, Clase_actividad) values ('Reto Fin de Año Bici 2023', 200, '1/12/2023', '1/1/2024', 'Fondo', 'Ciclismo')
+Insert into RETOS (Nombre, Kms_total, Fecha_inicio, Fecha_fin, Fondo_altura, Clase_actividad) values ('Reto Fin de Año Correr 2023', 100, '2023-12-01', '2024-01-01', 'Fondo', 'Correr')
+Insert into RETOS (Nombre, Kms_total, Fecha_inicio, Fecha_fin, Fondo_altura, Clase_actividad) values ('Reto Fin de Año Bici 2023', 200, '2023-12-01', '2024-01-01', 'Fondo', 'Ciclismo')
 
 Insert into PATROCINA_RETO (Nmbr_patrocinador, Nmbr_reto) values ('Inca Cola', 'Reto Fin de Año Correr 2023')
 Insert into PATROCINA_RETO (Nmbr_patrocinador, Nmbr_reto) values ('Inca Cola', 'Reto Fin de Año Bici 2023')
